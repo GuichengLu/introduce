789140cf39db10748dfe18d8f1d1b0b89f8500d7
Edward7503 <18313007503@163.com>
2017-06-17 03:26:37 +0800

    --all

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Edward on 2017/6/12.
+ */
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'vertical',
+    touchRatio : 1.5
+});
+var swiper = new Swiper('.swiper-container2', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'horizontal',
+    touchRatio : 1.5,
+    prevButton:'.swiper-button-prev',
+    nextButton:'.swiper-button-next',
+});
